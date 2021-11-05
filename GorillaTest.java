b8959ee0484fb6b0b7babcbd590a2ebf65d24446
Col-R <2colewestin@gmail.com>
2021-11-05 06:45:41 -0500

    addeed some methods to display

--- GorillaTest.java
+++ GorillaTest.java
@@ -2,5 +2,12 @@ public class GorillaTest {
     public static void main(String[] args) {
         Gorilla g = new Gorilla();
         g.displayEnergy();
+        g.eatBanana();
+        g.throwThings();
+        g.throwThings();
+        g.throwThings();
+        g.eatBanana();
+        g.climb();
+
     }
 }
